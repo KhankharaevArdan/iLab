43c6a8c38114a95e9ffc160ff4b44b8b54ae527e
KhankharaevArdan <71607932+KhankharaevArdan@users.noreply.github.com>
2020-10-03 00:56:37 +0300

    Add files via upload

--- square_solver1.c
+++ square_solver1.c
@@ -1,6 +1,4 @@
 
-// "Square equation solver"  //
-
 
 //===================================================================================
 
@@ -12,10 +10,10 @@
 
 //===================================================================================
 
-const double ACCURACY = 1E-5;
-const int INF_ROOTS = -1;
-const int NO_ROOTS = 0;
-const int NUMBER_OF_TESTS = 10;
+const double ACCURACY = 1E-5;   ///< Const for accuracy of calculations
+const int INF_ROOTS = -1;       ///< This const means infty roots
+const int NO_ROOTS = 0;         ///< This const means no roots
+const int NUMBER_OF_TESTS = 10; ///< Number of tests in unit test
 
 //===================================================================================
 
@@ -30,6 +28,13 @@ void switch_nRoots(int nRoots, double x1, double x2);
 //===================================================================================
 
 
+/*!
+    \brief Square equations solver
+    \version 1.0.0
+
+
+*/
+
 int main()
 {
     unit_test();
@@ -38,6 +43,45 @@ int main()
 
 //===================================================================================
 
+/*!
+    \brief This function solves linear equations
+    \param[in] b coefficient of linear equation bx + c = 0
+    \param[in] c coefficient of linear equation bx + c = 0
+    \param[out] x1 solution of linear equation bx + c = 0
+    \return Number of roots
+
+    \code
+    int linear_solver(double b, double c, double* x1)
+    {
+        assert(x1 != NULL);
+        assert(b  != NAN);
+        assert(c  != NAN);
+
+        if(is_zero(b) && is_zero(c))
+        {
+            return INF_ROOTS;
+        }
+
+        if(is_zero(b) && !is_zero(c))
+        {
+            return NO_ROOTS;
+        }
+
+        if(!is_zero(b) && is_zero(c))
+        {
+            *x1 = 0;
+            return 1;
+        }
+
+        if(!is_zero(b) && !is_zero(c))
+        {
+            *x1 = -c / b;
+            return 1;
+        }
+    return 3;
+    }
+    \endcode
+*/
 
 int linear_solver(double b, double c, double* x1)   //функция решения линейного уравнения
 {
@@ -71,6 +115,50 @@ return 3;
 
 //===================================================================================
 
+/*!
+    \brief This function solves quadratic equations
+    \param[in] a coefficient of quadratic equation
+    \param[in] b coefficient of quadratic equation
+    \param[in] c coefficient of quadratic equation
+    \param[out] x1 solution of quadratic equation
+    \param[out] x2 solution of quadratic equation
+    \return Number of roots
+
+    \code
+    int square_solver(double a, double b, double c, double* x1, double* x2)
+    {
+        assert(x1 != NULL);
+        assert(x2 != NULL);
+        assert(x1 !=   x2);
+        assert(a  !=  NAN);
+        assert(b  !=  NAN);
+        assert(c  !=  NAN);
+
+        double discr = b * b - 4 * a * c;
+
+        if(discr < 0)
+        {
+            return NO_ROOTS;
+        }
+
+        if(is_zero(discr))
+        {
+            *x1 = *x2 = -b / (2 * a);
+            return 1;
+        }
+
+        else
+        {
+            double sqrt_discr = sqrt(discr);
+            *x1 = (-b - sqrt_discr) / (2 * a);
+            *x2 = (-b + sqrt_discr) / (2 * a);
+            assert(x1 != x2);
+            return 2;
+        }
+
+    }
+    \endcode
+*/
 
 int square_solver(double a, double b, double c, double* x1, double* x2)     // функция решения квадратного уравнения
 {
@@ -107,6 +195,21 @@ int square_solver(double a, double b, double c, double* x1, double* x2)     // ф
 
 //===================================================================================
 
+/*!
+    \brief
+    \param[in] value
+    \return 0 if value >= ACCURACY
+            1 if value < ACCURACY
+
+    \code
+    int is_zero(double value)
+    {
+        if(fabs(value) >= ACCURACY)
+        return 0;
+    return 1;
+    }
+    \endcode
+*/
 
 int is_zero(double value)   // функция сравнения с нулем
 {
@@ -117,6 +220,9 @@ return 1;         //if value == 0
 
 //===================================================================================
 
+/*!
+    \brief Unit test
+*/
 
 void unit_test()
 {
@@ -124,8 +230,6 @@ void unit_test()
     FILE* output = fopen("output.txt", "w");
 
     assert(input != NULL);
-
-    output = fopen("output.txt", "w");
 
     int i = 0;
     int test_failed = 0;
@@ -195,6 +299,42 @@ void unit_test()
 
 //===================================================================================
 
+/*!
+    \brief manual test
+    \param[in] a coefficient of quadratic equation
+    \param[in] b coefficient of quadratic equation
+    \param[in] c coefficient of quadratic equation
+
+    \code
+    int manual_test()
+    {
+        double a = NAN, b = NAN, c = NAN;
+        printf("\n"
+               "Square equation solver\n"
+               "Enter 3 coefficients of squad equation a, b, c:\n");
+        scanf("%lg %lg %lg", &a, &b, &c);
+
+        if(scanf("%lg %lg %lg", &a, &b, &c) != 3)
+        {
+            printf("ERROR\n");
+            return 0;
+        }
+        assert(a != NAN);
+        assert(b != NAN);
+        assert(c != NAN);
+
+        double x1 = NAN, x2 = NAN;
+        int nRoots = 0;
+
+        if(!is_zero(a))
+            nRoots = square_solver(a, b, c, &x1, &x2);
+        else
+            nRoots = linear_solver(b, c, &x1);
+        switch_nRoots(nRoots, x1, x2);
+    return 0;
+    }
+    \endcode
+*/
 
 int manual_test()
 {
@@ -225,6 +365,37 @@ return 0;
 }
 
 //===================================================================================
+
+/*!
+    \brief This function prints solutions
+    \param[in] nRoots number of roots
+    \param[out] x1 solution of quadratic equation
+    \param[out] x2 solution of quadratic equation
+
+    \code
+    void switch_nRoots(int nRoots, double x1, double x2)
+    {
+        switch(nRoots)
+        {
+            case 0:         printf("No roots!\n");
+                            break;
+
+            case 1:         printf("1 root: x = %lg\n", x1);
+                            break;
+
+            case 2:         printf("2 roots: x1 = %lg, x2 = %lg\n", x1, x2);
+                            break;
+
+            case INF_ROOTS: printf("INFTY ROOTS\n");
+                            break;
+
+            default:        printf("ERROR");
+                            break;
+        }
+    }
+    \endcode
+*/
+
 void switch_nRoots(int nRoots, double x1, double x2)
 {
     switch(nRoots)
